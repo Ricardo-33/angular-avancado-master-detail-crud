e1590128fe9865c0aeba9c542826b760d003efc7
Ricardo-33 <edermask21@gmail.com>
2022-01-05 16:07:07 -0300

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,15 +1,19 @@
 import { InMemoryDbService, RequestInfo } from "angular-in-memory-web-api";
 import { Observable } from "rxjs";
 
+import { Category } from "./pages/categories";
+
 export class InMemoryDatabase implements InMemoryDbService {
+    /* InMemoryDbService pricisa de um método createDb para criar um objeto
+    in-memory database */
     createDb() {
-        const categories = [
+        const categories: Category[] = [
             { id: 1, name: 'Moradia', description: 'Pagamentos de Contas de Casa'},
             { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios'},
             { id: 3, name: 'Lazer', description: 'Cinema, parques, surfar, etc'},
             { id: 4, name: 'Salário', description: 'Recebimento de Salário'},
             { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer'}
         ]
-        return { categories}
+        return { categories }
     }
 }
--- src/app/pages/categories/index.ts
+++ src/app/pages/categories/index.ts
@@ -1,2 +1,5 @@
 export * from './categories-routing.module';
 export * from './categories.module';
+export * from './category-form';
+export * from './category-list';
+export * from './shared';
